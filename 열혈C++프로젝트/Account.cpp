b0aa8368b3e8fe2e30c487cf6d7fde6793b6ccba
copyrat90 <copyrat90@gmail.com>
2018-10-15 16:05:51 +0900

    02단계: 클래스로 변경

---
+++ 열혈C++프로젝트/Account.cpp
@@ -0,0 +1,38 @@
+// Account class Implementation
+// ver 0.2
+// Last Update 2018.10.15
+
+
+#include "pch.h"
+#include "Account.h"
+
+
+Account::Account(int accountID, char* name, int balance)
+	:accountID(accountID), balance(balance)
+{
+	if (balance < 0)
+	{
+		this->balance = 0;
+	}
+
+	this->name = new char[strlen(name) + 1];
+	strcpy_s(this->name, sizeof(char) * (strlen(name) + 1), name);
+}
+
+
+bool Account::Withdraw(int value)
+{
+	if (value > balance)
+		return false;
+
+	balance -= value;
+	return true;
+}
+
+
+void Account::ShowAccountInfo()
+{
+	std::cout << "°èÁÂID: " << accountID << std::endl;
+	std::cout << "ÀÌ  ¸§: " << name << std::endl;
+	std::cout << "ÀÜ  ¾×: " << balance << std::endl;
+}
---
+++ 열혈C++프로젝트/Account.h
@@ -0,0 +1,31 @@
+// Account class
+// ver 0.2
+// Last Update 2018.10.15
+
+
+#ifndef ACCOUNT_H
+#define ACCOUNT_H
+
+#include "pch.h"
+
+class Account
+{
+public:
+	Account(int accountID, char* name, int balance = 0);
+
+	void ShowAccountInfo();
+
+	int GetID() const { return accountID; }
+
+	void Deposit(int value) { balance += value; }
+	bool Withdraw(int value);
+
+	~Account() { delete[] name;	}
+
+private:
+	int accountID;
+	char *name;
+	int balance;
+};
+
+#endif // ACCOUNT_H
